6cfeb7643970b62984f57eb256e863daaa999823
“Edu58” <edumuriithi58@gmail.com>
2022-08-05 14:12:06 +0300

    adds the ACTION obj to reducing error margin

--- src/App.js
+++ src/App.js
@@ -15,22 +15,31 @@ function App() {
   //   setNumber(prev => prev - 1)
   // }
 
+
+  // To reduce chances of mispelling the action type and encoutering errors
+  // Can be defined in a separate file
+  const ACTION = {
+    ADD: 'add',
+    SUBTRACT: 'subtract',
+    ADDINPUT: 'addInput'
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <div className='counter'>
 
           <div style={{marginBottom:'2rem'}}>
-            <input type='text' placeholder='enter any text' onChange={(e) => dispatch({type: 'addInput', payload: e.target.value})}></input>
+            <input type='text' placeholder='enter any text' onChange={(e) => dispatch({type: ACTION.ADDINPUT, payload: e.target.value})}></input>
           </div>
 
           <p>{state.input}</p>
 
-          <button type='button' onClick={() => dispatch({ type: 'add' })}>+</button>
+          <button type='button' onClick={() => dispatch({ type: ACTION.ADD })}>+</button>
 
           <p className='count'>{state.count}</p>
 
-          <button type='button' onClick={() => dispatch({ type: 'subtract' })}>-</button>
+          <button type='button' onClick={() => dispatch({ type: ACTION.SUBTRACT })}>-</button>
 
         </div>
       </header>
